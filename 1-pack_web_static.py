31d8a462b7ee084588a56958cd2299ab35976761
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-08-27 20:16:19 -0500

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     """Function that generate the tgz file"""
 
     date = datetime.strftime("%Y%m%d%H%M%S")
-    file_tar = '/versions/web_static_{}.tgz'.format(date)
+    file_tar = 'versions/web_static_{}.tgz'.format(date)
     local('mkdir -p versions')
     try:
         local('tar -cvzf {} ~/AirBnB_clone_v2/web_static/'.format(file_tar))
