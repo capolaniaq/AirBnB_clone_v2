0c81a136c73840b2fe40bef7ae12109195a54c97
capolaniaq <capolaniaq@correo.udistrital.edu.co>
2021-09-11 16:07:44 -0500

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Script that """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """function that return message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """function that return message in route /hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_message(text):
+    """function that return message in route /c/
+    with variable inside"""
+    string = text
+    string = string.replace('_', ' ')
+    return "C {}".format(string)
+
+
+@app.route("/python/", defaults={"text": "is cool"})
+@app.route("/python/<text>")
+def python_message(text):
+    """Function that send the message with the defaul value"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
